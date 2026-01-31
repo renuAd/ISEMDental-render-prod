e7361d4ca163b849fbb603f95359e0372699db9e
renuAd <rhino.adame@gmail.com>
2026-01-31 13:21:30 +0800

    another build sh change

--- build.sh
+++ build.sh
@@ -6,13 +6,10 @@ python -m pip install --upgrade pip
 
 # Install dependencies
 python -m pip install -r requirements.txt
-
+cd isem
 # Collect static files
 python manage.py collectstatic --noinput
 
 # Apply migrations
 python manage.py migrate
 # Create a superuser if it doesn't exist
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='admin').exists(): \
-    User.objects.create_superuser('admin', 'rhino@gmail.com', 'admin123')" | python manage.py shell
